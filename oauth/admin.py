a414c4bcf212ad7180728a34ad3e799c14177b80
cjй <sergeygurin2003f@gmail.com>
2024-10-07 01:33:18 +0300

    [0.3] oauth -> admin.py

--- oauth/admin.py
+++ oauth/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+
+@admin.register(models.AuthUser)
+class AuthUserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'email', 'display_name', 'join_date')
+    list_display_links = ('email',)#Щоб клікать на емейл, а не на айді
+
+
+@admin.register(models.SocialLink)
+class SocialLinkAdmin(admin.ModelAdmin):
+    list_display = ('user', 'link')
